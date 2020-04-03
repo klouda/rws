ae4176c121009dcef9a426bec049f8934e9e7169
Jaroslav Klouda <jaroslav.klouda@gmail.com>
2020-04-03 13:00:34 +0200

    add sources

--- pageTest/pageTest/src/test/java/PageTest.java
+++ pageTest/pageTest/src/test/java/PageTest.java
@@ -1,51 +1,57 @@
 import java.util.List;
+import java.lang.*;
 
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-
-    /**
+/**
      * @author jklouda
      * @since 28.03.2020
      */
 
-    public class PageTest {
-        public static void main (String[] args) {}
-        WebDriver driver=new FirefoxDriver();
+public class PageTest {
+    public static void main (String[] args)  {
+        System.setProperty("webdriver.chrome.driver", "C://chromedriver//chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        openAndSearchTextOnWikiPage(driver);
+    }
 
-        @Test
-        public void openAndSearchTextonWikiPage() {
-            final String wikiPage = "https://en.wikipedia.org/wiki/Philosophical_theory";
-            final String word = "Philosophy";
+    public static void openAndSearchTextOnWikiPage(WebDriver driver) {
+        final String wikiPage = "https://en.wikipedia.org/wiki/Philosophical_theory";
+        final String word = "Philosophy";
 
-            openWikiPage(wikiPage);
-            searchWordOnPage(word);
-        }
+        openWikiPage(driver, wikiPage);
+        searchWordOnPage(driver, word);
+        closeWikiPage(driver);
+    }
 
-        public void openWikiPage(final String wikiPage) {
-            driver.get(wikiPage);
-        }
+    public static void openWikiPage(WebDriver driver, String wikiPage) {
+        driver.get(wikiPage);
+    }
 
-        public void searchWordOnPage(final String word) {
-            int cnt = 0;
-            List<WebElement> links = driver.findElements(By.tagName("a"));
-            for (WebElement link : links)
+    public static void searchWordOnPage(WebDriver driver, String word) {
+        int cnt = 0;
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        for (WebElement link : links)
+        {
+            if (link.getAttribute("title").contains(word))
             {
-                if (link.getAttribute("title").contains(word))
-                {
-                    link.click();
-                    System.out.println("Link contains word " + word + ".");
-                    System.out.println(cnt++);
-                    break;
-                }
-                else
-                {
-                    System.out.println("Link does not contains word " + word + ".");
-                    System.out.println(cnt++);
-                }
+                link.click();
+                System.out.println("Link contains word " + word + ".");
+                System.out.println(cnt++);
+                break;
+            }
+            else {
+                System.out.println("Link does not contains word " + word + ".");
+                System.out.println(cnt++);
             }
         }
     }
+
+    public static void closeWikiPage(WebDriver driver) {
+        driver.close();
+    }
+}
